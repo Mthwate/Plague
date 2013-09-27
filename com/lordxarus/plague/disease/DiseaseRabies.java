000814b972f9bdf042b86c6e5173318db4f67457
Matthew Benkart <mthwate@mthwate.com>
2013-09-26 22:12:05 -0400

    Rabies now spreads randomly and spreads to nearby entities

--- com/lordxarus/plague/disease/DiseaseRabies.java
+++ com/lordxarus/plague/disease/DiseaseRabies.java
@@ -1,5 +1,6 @@
 package com.lordxarus.plague.disease;
 
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 
@@ -18,19 +19,54 @@ import com.lordxarus.plague.ModLogger;
 public class DiseaseRabies extends Disease {
 	
 	public void entityUpdate(LivingUpdateEvent event) {
+		Entity entity = event.entity;
+		if(isVulnerable(entity)) {
+			if (DiseaseHelper.isDiseaseActive(entity, this)) {
+				effect(entity);
+				spread(entity);
+			} else if (!DiseaseHelper.isDiseaseActive(entity, this)) {
+				contract(entity);
+			}
+		}
+	}
+	
+	void effect(Entity entity) {
+		
+	}
+	
+	void spread(Entity entityCarrier) {
+		List<Entity> entities = entityCarrier.worldObj.getEntitiesWithinAABBExcludingEntity(entityCarrier, entityCarrier.boundingBox.expand(5.0D, 5.0D, 5.0D));
+		for (Entity entityTarget : entities) {
+			if (isVulnerable(entityTarget) && !DiseaseHelper.isDiseaseActive(entityTarget, this)) {
+				Random rand = new Random();
+				if (rand.nextInt(100000) == 0) {
+					DiseaseHelper.addDisease(entityCarrier, this);
+					ModLogger.log(Level.INFO, entityTarget.getEntityName() + " contracted rabies from " + entityCarrier.getEntityName() + "!", true);
+				}
+			}
+		}
+		
+	}
+	
+	void contract(Entity entity) {
+		Random rand = new Random();
+		if (rand.nextInt(100000000) == 0) {
+			DiseaseHelper.addDisease(entity, this);
+			ModLogger.log(Level.INFO, entity.getEntityName() + " contracted rabies!", true);
+		}
 	}
 	
 	public void entityAttack(LivingAttackEvent event) {
 		Entity entityVictim = event.entity;
 		Entity entityAttacker = event.source.getEntity();
-		if (isVulnerable(entityVictim)) {
+		if (isVulnerable(entityVictim) && !DiseaseHelper.isDiseaseActive(entityVictim, this)) {
 			if (event.source.getEntity() != null) {
 				if (event.source.getEntity() instanceof EntityLiving) {
 					if(DiseaseHelper.isDiseaseActive(entityAttacker, this)) {
 						Random rand = new Random();
 						if (rand.nextInt(100) == 0) {
 							DiseaseHelper.addDisease(entityVictim, this);
-							ModLogger.log(Level.INFO, entityVictim.getEntityName() + " contracted rabies from " + entityAttacker.getEntityName(), true);
+							ModLogger.log(Level.INFO, entityVictim.getEntityName() + " contracted rabies from " + entityAttacker.getEntityName() + "!", true);
 						}
 					}
 				}
