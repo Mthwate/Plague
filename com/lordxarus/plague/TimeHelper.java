52ad72180649a1ac2ef6530a6549b28c892c714c
Matthew Benkart <mthwate@mthwate.com>
2013-09-28 17:20:54 -0400

    Expanded functionality of TimeHelper

--- com/lordxarus/plague/TimeHelper.java
+++ com/lordxarus/plague/TimeHelper.java
@@ -14,4 +14,32 @@ public class TimeHelper {
 		return((hour * 60 * 60 * 20) + (min * 60 * 20) + (sec * 20));
 	}
 	
+	public static int tickToTime(int ticks) {
+		return(ticks/20);
+	}
+	
+	public static double mcToTime(int sec) {
+		return(mcToTime(0, 0, sec));
+	}
+	
+	public static double mcToTime(int min, int sec) {
+		return(mcToTime(0, min, sec));
+	}
+	
+	public static double mcToTime(int hour, int min, int sec) {
+		return((hour * 50) + (min * (50.0/60.0)) + (sec * (50.0/(60.0 * 60.0))));
+	}
+	
+	public static int timeToMc(int sec) {
+		return(timeToMc(0, 0, sec));
+	}
+	
+	public static int timeToMc(int min, int sec) {
+		return(timeToMc(0, min, sec));
+	}
+	
+	public static int timeToMc(int hour, int min, int sec) {
+		return((hour * 60 * 60 * 72) + (min * 60 * 72) + (sec * 72));
+	}
+	
 }
