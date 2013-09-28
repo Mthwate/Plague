6a55ed15f2bd832bf7809c20cd6125d5fd43f1d7
Matthew Benkart <mthwate@mthwate.com>
2013-09-27 21:14:28 -0400

    Added getBlock function

--- com/lordxarus/plague/EntityHelper.java
+++ com/lordxarus/plague/EntityHelper.java
@@ -1,5 +1,8 @@
 package com.lordxarus.plague;
 
+import java.util.logging.Level;
+
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.biome.BiomeGenBase;
 
@@ -7,6 +10,24 @@ public class EntityHelper {
 	
 	public static BiomeGenBase getBiome(Entity entity) {
 		return(entity.worldObj.getWorldChunkManager().getBiomeGenAt((int) entity.posX, (int) entity.posZ));
+	}
+	
+	public static boolean getBlock(Entity entity, int radius, Block block) {
+		int eX = (int) entity.posX;
+		int eY = (int) entity.posY;
+		int eZ = (int) entity.posZ;
+		
+		for (int x = eX - radius; x <= eX + radius; x++) {
+			for (int y = eY - radius; y <= eY + radius; y++) {
+				for (int z = eZ - radius; z <= eZ + radius; z++) {
+					if (entity.worldObj.getBlockId(x, y, z) == block.blockID) {
+						return(true);
+					}
+				}
+			}
+		}
+		
+		return(false);
 	}
 
 }
