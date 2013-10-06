6f8c2efd9fa4b29de711fa6909ac3d3370bb3148
Matthew Benkart <mthwate@mthwate.com>
2013-10-06 10:07:23 -0400

    The disease vile now has a creative tab

--- com/lordxarus/plague/item/ItemDiseaseVileEmpty.java
+++ com/lordxarus/plague/item/ItemDiseaseVileEmpty.java
@@ -1,10 +1,13 @@
 package com.lordxarus.plague.item;
 
+import net.minecraft.creativetab.CreativeTabs;
+
 public class ItemDiseaseVileEmpty extends ItemBase {
 	
 	public ItemDiseaseVileEmpty(int id) {
 		super(id);
 		this.maxStackSize = 1;
+		this.setCreativeTab(CreativeTabs.tabMisc);
 	}
 
 }
