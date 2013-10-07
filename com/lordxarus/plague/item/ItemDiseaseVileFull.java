3f856ab1ca24d214317cdf8fe195c9c81c6dfa81
Matthew Benkart <mthwate@mthwate.com>
2013-10-06 20:12:32 -0400

    Added tooltips for the disease filled vile that shows the percent extracted and analyzed

--- com/lordxarus/plague/item/ItemDiseaseVileFull.java
+++ com/lordxarus/plague/item/ItemDiseaseVileFull.java
@@ -1,9 +1,18 @@
 package com.lordxarus.plague.item;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.List;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+
+import com.lordxarus.plague.lib.TimeHelper;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemDiseaseVileFull extends ItemBase {
 	
@@ -18,6 +27,26 @@ public class ItemDiseaseVileFull extends ItemBase {
 			if ((!complete) && (entity instanceof EntityPlayer)) {
 				stack.getTagCompound().setBoolean("complete", true);
 			}
+		}
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List dataList, boolean bool) {
+		if (itemStack.getTagCompound() != null) {
+			double extractorDuration = TimeHelper.tickToMc(itemStack.getTagCompound().getInteger("extractorDuration"));
+			double analyzerDuration = TimeHelper.tickToMc(itemStack.getTagCompound().getInteger("analyzerDuration"));
+			
+			extractorDuration = extractorDuration / (60 * 60);
+			analyzerDuration = analyzerDuration / (60 * 60);
+			
+			DecimalFormat df = new DecimalFormat("#.##");
+			df.setRoundingMode(RoundingMode.FLOOR);
+			
+			String roundedExtractorDuration = df.format(extractorDuration);
+			String roundedAnalyzerDuration = df.format(analyzerDuration);
+			
+			dataList.add(roundedExtractorDuration + "% Extracted");
+			dataList.add(roundedAnalyzerDuration + "% Analyzed");
 		}
 	}
 
