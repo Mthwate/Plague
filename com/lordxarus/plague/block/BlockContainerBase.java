729bd991e7780ae5bb3e29323a2f8f5ce9320caf
Matthew Benkart <mthwate@mthwate.com>
2013-11-08 12:07:12 -0500

    Machines now rotate to face the player when they are placed

--- com/lordxarus/plague/block/BlockContainerBase.java
+++ com/lordxarus/plague/block/BlockContainerBase.java
@@ -1,13 +1,14 @@
 package com.lordxarus.plague.block;
 
-import java.util.List;
-
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 import com.lordxarus.plague.Plague;
@@ -42,12 +43,24 @@ public class BlockContainerBase extends BlockContainer {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public Icon getIcon(int side, int metadata) {
-		if(side == 2) {
-			return frontIcon;
-		} else if((side == 0) || (side == 1)) {
+		if((side == 0) || (side == 1)) {
 			return topBottomIcon;
+		} else if (metadata == 2 && side == 2) {
+			return frontIcon;
+		} else if (metadata == 3 && side == 5) {
+			return frontIcon;
+		} else if (metadata == 0 && side == 3) {
+			return frontIcon;
+		} else if (metadata == 1 && side == 4) {
+			return frontIcon;
 		} else {
 			return sideIcon;
 		}
+	}
+	
+	@Override
+	public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack) {
+		int direction = MathHelper.floor_double((double)(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+		par1World.setBlockMetadataWithNotify(x, y, z, direction, 2);
 	}
 }
